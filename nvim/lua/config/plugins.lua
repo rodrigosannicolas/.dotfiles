c2807dfe3a75d65955c3ebc9569ebf5934d378f7
Rodrigo San Nicolas <rodrigosannicolas@gmail.com>
2022-10-17 12:35:26 -0300

    Adjust shift width

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -1,18 +1,18 @@
 return require("packer").startup(function(use)
   -- Packer can manage itself
-	use "wbthomason/packer.nvim"
+  use "wbthomason/packer.nvim"
 
   -- Colorscheme
-	use "ayu-theme/ayu-vim"
+  use "ayu-theme/ayu-vim"
 
 	-- File explorer
-	use { "nvim-tree/nvim-tree.lua", requires = { "nvim-tree/nvim-web-devicons" } }
+  use { "nvim-tree/nvim-tree.lua", requires = { "nvim-tree/nvim-web-devicons" } }
 
 	-- Parser generator tool
   use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
 	-- Fuzzy finder
-	use { "nvim-telescope/telescope.nvim", tag = "0.1.0", requires = { { "nvim-lua/plenary.nvim" } } }
+  use { "nvim-telescope/telescope.nvim", tag = "0.1.0", requires = { { "nvim-lua/plenary.nvim" } } }
 
   -- Airline
   use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
