4a020c38e07db2e5ddd6719bb10fddcc3480062f
Rodrigo San Nicolas <rodrigosannicolas@gmail.com>
2023-02-15 16:17:12 -0300

    Update lualine

--- nvim/.config/nvim/lua/config/lualine.lua
+++ nvim/.config/nvim/lua/config/lualine.lua
@@ -20,21 +20,13 @@ require('lualine').setup {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
+    lualine_c = {{ 'filename', path = 1 }},
     lualine_x = { 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' }
   },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { 'filename' },
-    lualine_x = { 'location' },
-    lualine_y = {},
-    lualine_z = {}
-  },
   tabline = {
-    lualine_a = {{ 'tabs', max_length=vim.o.columns, mode = 1 }},
+    lualine_a = {{ 'tabs', max_length = vim.o.columns, mode = 1 }},
     lualine_b = {},
     lualine_c = {},
     lualine_x = {},
