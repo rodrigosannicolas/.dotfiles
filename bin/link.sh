0ba6ebfa7929a329ba06a43b3577988b6844317d
Rodrigo San Nicolas <rodrigosannicolas@gmail.com>
2023-03-09 10:34:31 -0300

    Update dotfiles

--- bin/link.sh
+++ bin/link.sh
@@ -2,23 +2,19 @@
 
 echo "Creating symlinks..."
 
-if [ -e $HOME/.alacritty.yml ]; then rm -rf $HOME/.alacritty.yml; fi
-ln -s $HOME/.dotfiles/alacritty/.alacritty.yml $HOME/.alacritty.yml
+create_symlink () {
+  if [ -e $HOME/$2 ]; then rm -rf $HOME/$2; fi
+  ln -s $HOME/.dotfiles/$1 $HOME/$2
+}
 
-if [ -e $HOME/.gitconfig ]; then rm -rf $HOME/.gitconfig; fi
-ln -s $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
-
-if [ -d $HOME/.config/nvim ]; then rm -rf $HOME/.config/nvim; fi
-ln -s $HOME/.dotfiles/nvim $HOME/.config/nvim
-
-if [ -e $HOME/.rubocop.yml ]; then rm -rf $HOME/.rubocop.yml; fi
-ln -s $HOME/.dotfiles/rubocop/.rubocop.yml $HOME/.rubocop.yml
-
-if [ -e $HOME/.skhdrc ]; then rm -rf $HOME/.skhdrc; fi
-ln -s $HOME/.dotfiles/skhd/.skhdrc $HOME/.skhdrc
-
-if [ -e $HOME/.tmux.conf ]; then rm -rf $HOME/.tmux.conf; fi
-ln -s $HOME/.dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
-
-if [ -e $HOME/.yabairc ]; then rm -rf $HOME/.yabairc; fi
-ln -s $HOME/.dotfiles/yabai/.yabairc $HOME/.yabairc
+create_symlink "alacritty/.alacritty.yml" ".alacritty.yml"
+create_symlink "git/.gitconfig"           ".gitconfig"
+create_symlink "i3/config"                ".config/i3/config"
+create_symlink "nvim"                     ".config/nvim"
+create_symlink "p10k/.p10k.zsh"           ".p10k.zsh"
+create_symlink "rubocop/.rubocop.yml"     ".rubocoop.yml"
+create_symlink "skhd/.skhdrc"             ".skhdrc"
+create_symlink "tmux/.tmux.conf"          ".tmux.tmux"
+create_symlink "yabai/.yabairc"           ".yabairc"
+create_symlink "zsh/.zsh_aliases"         ".zsh_aliases"
+create_symlink "zsh/.zshrc"               ".zshrc"
