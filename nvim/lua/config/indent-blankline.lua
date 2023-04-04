00cacccde3a0044284980a7eda006d10e5c6f322
Rodrigo San Nicolas <rodrigosannicolas@gmail.com>
2023-04-04 09:16:37 -0300

    Make nvim config a git submodule

--- nvim/lua/config/indent-blankline.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#2C2E43 gui=nocombine]]
-
-require("indent_blankline").setup {
-    space_char_blankline = " ",
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-    },
-}
